0ef608d9d0042b731376645622b4489962f14f94
olafur-palsson <olafur.palsson2@gmail.com>
2019-02-28 14:12:42 +0000

    lol

--- SpringBootMVC/src/main/java/project/GreetingController.java
+++ SpringBootMVC/src/main/java/project/GreetingController.java
@@ -1,10 +1,9 @@
 package project;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class GreetingController {
 
