1b8b93d4f1d845cffee78db842486cc78ca0dd49
olafur-palsson <olafur.palsson2@gmail.com>
2019-02-28 13:02:36 +0000

    debug

--- SpringBootMVC/src/main/java/project/controller/EventController.java
+++ SpringBootMVC/src/main/java/project/controller/EventController.java
@@ -9,12 +9,7 @@ import org.json.JSONObject;
 import org.springframework.ui.Model;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+mport org.springframework.web.bind.annotation.*;
 
 import project.controller.Toolkit;
 import project.persistence.entities.*;
--- SpringBootMVC/src/main/java/project/controller/HomeController.java
+++ SpringBootMVC/src/main/java/project/controller/HomeController.java
@@ -1,11 +1,8 @@
 package project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMethod;
 import project.service.StringManipulationService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import project.controller.Toolkit;
 
